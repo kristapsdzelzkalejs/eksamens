d072a4aaa1ad613fcda4c9f37cce04b7d4181a0d
Kristaps <kristaps.dzelzkalejs@ossnet.lv>
2021-02-20 10:02:05 +0200

    Edit rinka_laukums.py code

--- rinka_laukums.py
+++ rinka_laukums.py
@@ -1,6 +1,4 @@
 pi = 3.14
 radius = int(input('Ievadi rinka radiusu :'))
 laukums = pi * radius * radius
-print("Area of the circle is : {}".format(laukums))
-
-#print ("Rinka laukums ir " + str(radius) + " is: " + str(pi * radius**2))
+print("Apla laukums ir: {}".format(laukums))
