aea77abbf9ff6f3bb93c70b941bb6295c6dd1fe0
Kristaps <kristaps.dzelzkalejs@ossnet.lv>
2021-02-20 09:58:48 +0200

    Add correct solution to task 10_skaitlu_summa

--- 10_skaitlu_summa.py
+++ 10_skaitlu_summa.py
@@ -1,8 +1,3 @@
-def uz_skaitli(text, default=0):
-    try:
-        return int(text)
-    except ValueError:
-        return default
-
 skaitli = input("Ievadi 10 skaitlus atdalitus ar atstarpi: ")
-print(sum(cipars for word in skaitli for cipars in map(uz_skaitli, word.split())))
+a = list(map(int, skaitli.split(" ")))
+print(sum(a))
