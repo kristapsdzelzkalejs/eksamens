7a7c22e71737ba2fccdd8f1425295ea78d65aef4
Kristaps <kristaps.dzelzkalejs@ossnet.lv>
2021-02-20 10:03:15 +0200

    Edit spelling mistakes

--- no_100_1000.py
+++ no_100_1000.py
@@ -3,4 +3,4 @@ while skaitlis < 100 or skaitlis > 1000:
     print("Arpus diapazona!!")
     skaitlis = int(input("Ievadi skaitli no 100 lidz 1000:"))
 else:
-    print("Tavs ievadis skaitlis ir {0}".format(skaitlis))
+    print("Tavs ievaditais skaitlis ir {0}".format(skaitlis))
